f12264e29c4d0789e8a5d7d0af0f2eeb6c371eee
Revaz-Gelashvili <138714567+Revaz-Gelashvili@users.noreply.github.com>
2025-05-04 04:37:01 +0400

    4:36 AM

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-<script
-  src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-  integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-  crossorigin="anonymous"
-></script>;
+window.addEventListener("load", () => {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const closeBtn = document.querySelector(".close-btn");
+  const sidebar = document.querySelector(".sidebar");
+
+  if (menuToggle && closeBtn && sidebar) {
+    menuToggle.addEventListener("click", () => {
+      sidebar.classList.add("active");
+    });
+
+    closeBtn.addEventListener("click", () => {
+      sidebar.classList.remove("active");
+    });
+  } else {
+    console.warn("Один или несколько элементов не найдены в DOM.");
+  }
+});
